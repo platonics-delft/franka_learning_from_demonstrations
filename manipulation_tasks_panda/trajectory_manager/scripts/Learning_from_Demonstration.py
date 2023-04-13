93cc7a0ac69aa3977ea762f627a7b83c3afeeb81
platonics <nano2598.r@gmail.com>
2023-04-13 12:00:19 +0200

    Refactoring goal construction

--- manipulation_tasks_panda/trajectory_manager/scripts/Learning_from_Demonstration.py
+++ manipulation_tasks_panda/trajectory_manager/scripts/Learning_from_Demonstration.py
@@ -142,18 +142,11 @@ class Learning_from_Demonstration():
             self.color_image_sub = rospy.Subscriber('camera/color/image_raw', Image, self.color_image_callback)
 
     def home(self):
-        goal = PoseStamped()
+        pos_array = np.array([0.6, 0, 0.4])
+        quat = np.quaternion(0, 1, 0, 0)
+        goal = array_quat_2_pose(pos_array, quat)
         goal.header.seq = 1
         goal.header.stamp = rospy.Time.now()
-        goal.header.frame_id = "map"
-        goal.pose.position.x = 0.6
-        goal.pose.position.y = 0
-        goal.pose.position.z = 0.4
-
-        goal.pose.orientation.w = 0
-        goal.pose.orientation.x = 1
-        goal.pose.orientation.y = 0
-        goal.pose.orientation.z = 0
 
         ns_msg = [0, 0, 0, -2.4, 0, 2.4, 0]
 
@@ -247,24 +240,11 @@ class Learning_from_Demonstration():
 
             self.r.sleep()
 
-        goal = PoseStamped()
-        goal.header.seq = 1
-        goal.header.stamp = rospy.Time.now()
-        goal.header.frame_id = "map"
-
-        goal.pose.position.x = self.curr_pos[0]
-        goal.pose.position.y = self.curr_pos[1]
-        goal.pose.position.z = self.curr_pos[2]
-        
-        goal.pose.orientation.w = self.curr_ori[0]
-        goal.pose.orientation.x = self.curr_ori[1]
-        goal.pose.orientation.y = self.curr_ori[2]
-        goal.pose.orientation.z = self.curr_ori[3]
+        quat_goal = list_2_quaternion(self.curr_ori)
+        goal = array_quat_2_pose(self.curr_pos, quat_goal)
         self.goal_pub.publish(goal)
         self.set_stiffness(100, 100, 100, 5, 5, 5, 0)
         rospy.loginfo("Ending trajectory recording")
-        self.recorded_traj = savgol_filter(self.recorded_traj, 51, 3)
-        self.recorded_ori = savgol_filter(self.recorded_ori, 51, 3)
 
     # control robot to desired goal position
     def go_to_pose(self, goal_pose, interp_dist=0.001/1.5, interp_dist_polar=0.001/1.5): ##### Are both interpolation distances needed?
@@ -307,16 +287,9 @@ class Learning_from_Demonstration():
         goal = PoseStamped()
         self.set_stiffness(4000, 4000, 4000, 30, 30, 30, 40)
         for i in range(step_num):
-
-            goal.pose.position.x = x[i]
-            goal.pose.position.y = y[i]
-            goal.pose.position.z = z[i]
             quat=np.slerp_vectorized(q_start, q_goal, i/step_num)
-            
-            goal.pose.orientation.x = quat.x
-            goal.pose.orientation.y = quat.y
-            goal.pose.orientation.z = quat.z
-            goal.pose.orientation.w = quat.w
+            pos_array = np.array([x[i], y[i], z[i]])
+            goal = array_quat_2_pose(pos_array, quat)
             self.goal_pub.publish(goal)
             self.r.sleep()
         rospy.sleep(2.0)
@@ -329,14 +302,8 @@ class Learning_from_Demonstration():
         print('entered execute')
         start = PoseStamped()
 
-        start.pose.position.x = self.recorded_traj[0][0]
-        start.pose.position.y = self.recorded_traj[1][0]
-        start.pose.position.z = self.recorded_traj[2][0]
-
-        start.pose.orientation.w = self.recorded_ori[0][0]
-        start.pose.orientation.x = self.recorded_ori[1][0]
-        start.pose.orientation.y = self.recorded_ori[2][0]
-        start.pose.orientation.z = self.recorded_ori[3][0]
+        quat_start = list_2_quaternion(self.recorded_ori[:, 0])
+        start = array_quat_2_pose(self.recorded_traj[:, 0], quat_start)
         
         if self.pose_icp:
             transform = pose_st_2_transformation(self.pose_icp)
@@ -346,20 +313,10 @@ class Learning_from_Demonstration():
         i=0
         while i <( self.recorded_traj.shape[1]):
 
-            goal = PoseStamped()
-
+            quat_goal = list_2_quaternion(self.recorded_ori[:, i])
+            goal = array_quat_2_pose(self.recorded_traj[:, i], quat_goal)
             goal.header.seq = 1
             goal.header.stamp = rospy.Time.now()
-            goal.header.frame_id = "map"		
-            goal.pose.position.x = self.recorded_traj[0][i]
-            goal.pose.position.y = self.recorded_traj[1][i]
-            goal.pose.position.z = self.recorded_traj[2][i]
-
-            goal.pose.orientation.w = self.recorded_ori[0][i]
-            goal.pose.orientation.x = self.recorded_ori[1][i]
-            goal.pose.orientation.y = self.recorded_ori[2][i]
-            goal.pose.orientation.z = self.recorded_ori[3][i]
-            
             ori_threshold = 0.3
             pos_threshold = 0.1
             
