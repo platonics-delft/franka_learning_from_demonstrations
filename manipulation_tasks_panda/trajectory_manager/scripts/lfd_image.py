template matching fixed, decreased panda rate, transform now only allows rotation around z

manipulation_tasks_panda/trajectory_manager/scripts/lfd_image.py
@@ -18,6 +18,7 @@ from cv_bridge import CvBridgeError, CvBridge
 from lfd import LfD
 from visual_servoing import max_correlation, template_matching, template_matching_SSD
 import tf
+import quaternion
 import pdb
 
 width = int(1280/4)
@@ -55,6 +56,7 @@ class LfD_image(LfD):
         self.current_template_pub = rospy.Publisher('/current_template', Image, queue_size=0)
         self.corrected_goal_pub = rospy.Publisher('/corrected_goal', PoseStamped, queue_size=0)
         self.bridge = CvBridge()
+        self._tf_broadcaster = tf.TransformBroadcaster()
 
         self.curr_image = None
         self.recorded_traj = None
@@ -64,12 +66,16 @@ class LfD_image(LfD):
         self.width_ds = int(1280/4)
         self.end = False
 
-        self.row_crop_pct_top = 0.1
+        self.row_crop_pct_top = 0.3
         self.row_crop_pct_bot = 0.9
-        self.col_crop_pct_left = 0.5
-        self.col_crop_pct_right = 0.9
+        self.col_crop_pct_left = 0.6
+        self.col_crop_pct_right = 0.8
         self.row_bias_pct = (self.row_crop_pct_top + self.row_crop_pct_bot)/2 - 0.5
         self.col_bias_pct = (self.col_crop_pct_left + self.col_crop_pct_right)/2 - 0.5
+        self.ds_factor = 4
+
+        self.x_dist_threshold = 2
+        self.y_dist_threshold = 2
 
         ros_pack = rospkg.RosPack()
         self._package_path = ros_pack.get_path('trajectory_manager')
@@ -104,9 +110,14 @@ class LfD_image(LfD):
             vel = math.sqrt((self.curr_pos[0]-init_pos[0])**2 + (self.curr_pos[1]-init_pos[1])**2 + (self.curr_pos[2]-init_pos[2])**2)
         self.recorded_traj = self.curr_pos
         self.recorded_ori = self.curr_ori
+        if self.gripper_width < 0.03:
+            self.grip_value = 0
+        else:
+            self.grip_value = 0.04
+        self.recorded_gripper= self.grip_value
         self.recorded_gripper= self.grip_value
      
-        resized_img_gray=image_process(self.curr_image, 4,  self.row_crop_pct_top , self.row_crop_pct_bot,
+        resized_img_gray=image_process(self.curr_image, self.ds_factor,  self.row_crop_pct_top , self.row_crop_pct_bot,
                                         self.col_crop_pct_left, self.col_crop_pct_right)
         resized_img_msg = self.bridge.cv2_to_imgmsg(resized_img_gray)
         self.cropped_img_pub.publish(resized_img_msg)
@@ -118,7 +129,8 @@ class LfD_image(LfD):
             self.recorded_ori  = np.c_[self.recorded_ori, self.curr_ori]
             self.recorded_gripper = np.c_[self.recorded_gripper, self.grip_value]
 
-            resized_img_gray=image_process(self.curr_image, 4, 0.1, 0.9, 0.5, 0.9)
+            resized_img_gray=image_process(self.curr_image, self.ds_factor, self.row_crop_pct_top , self.row_crop_pct_bot,
+                                        self.col_crop_pct_left, self.col_crop_pct_right)
             resized_img_msg = self.bridge.cv2_to_imgmsg(resized_img_gray)
             self.cropped_img_pub.publish(resized_img_msg)
             self.recorded_img = np.r_[self.recorded_img, resized_img_gray.reshape((1, resized_img_gray.shape[0], resized_img_gray.shape[1]))]
@@ -157,9 +169,11 @@ class LfD_image(LfD):
             start = self.transform(start)
             
         self.go_to_pose(start)
-        self.set_stiffness(1000, 1000, 1000, 30, 30, 30, 0)
+        self.set_stiffness(2000, 2000, 2000, 30, 30, 30, 0)
 
         i=0
+        z_sum = 0
+
         while i <( self.recorded_traj.shape[1]):
             quat_goal = list_2_quaternion(self.recorded_ori[:, i])
             goal = array_quat_2_pose(self.recorded_traj[:, i], quat_goal)
@@ -184,75 +198,33 @@ class LfD_image(LfD):
                 self.move_gripper(self.recorded_gripper[0][i])
                 time.sleep(0.1)
 
-            resized_img_gray = image_process(self.curr_image, 4,  0, 1, 0, 1)
-            resized_img_gray_tensor = torch.from_numpy(resized_img_gray).unsqueeze(0).unsqueeze(0)
-            recorded_image_msg = self.bridge.cv2_to_imgmsg(self.recorded_img[i])
+            resized_img_gray = image_process(self.curr_image, self.ds_factor,  0, 1, 0, 1)
+
+            idx=np.argmin(np.linalg.norm(self.recorded_traj-(self.curr_pos).reshape(3,1),axis=0))
+            recorded_image_msg = self.bridge.cv2_to_imgmsg(self.recorded_img[idx])
 
             self.current_template_pub.publish(recorded_image_msg)  
-            methods = ['conv', 'corr_correlation', 'cv2']
-            method = methods[2]
-            if method=='conv':
-                max_corr=max_correlation(self.recorded_img_tensor[i])
-                xy_distance, max_val=template_matching(resized_img_gray_tensor, self.recorded_img_tensor[i].unsqueeze(0).unsqueeze(0))
+            res = cv2.matchTemplate(resized_img_gray, self.recorded_img[idx], cv2.TM_SQDIFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            top_left_xy = min_loc
+            x_rect = int(top_left_xy[0])
+            y_rect = int(top_left_xy[1])
 
-                xy_distance = xy_distance - np.array([self.col_bias, self.row_bias])
-                print(xy_distance)
-                x_rect = int(resized_img_gray.shape[1] / 2 + xy_distance[0] - self.recorded_img_tensor[i].shape[1] / 2)
-                y_rect = int(resized_img_gray.shape[0] / 2 + xy_distance[1] - self.recorded_img_tensor[i].shape[0] / 2)
-
-                w_rect = int(self.recorded_img_tensor[i].shape[1])
-                h_rect = int(self.recorded_img_tensor[i].shape[0])
-                uncertainty = (max_val/max_corr).detach().numpy()
-
-                # Draw the bounding box on the image
-                if uncertainty > 0.8:
-                    cv2.rectangle(resized_img_gray, (x_rect, y_rect), (x_rect + w_rect, y_rect + h_rect), (0, 255, 0), 2)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    font_scale = 0.8
-                    font_color = (0, 0, 255)
-                    thickness = 2            
-                    cv2.putText(resized_img_gray, str(uncertainty), (x_rect, int(y_rect+h_rect/2)), font, font_scale, font_color, thickness)
-
-            elif method=='conv_correlation':
-                x, y, max_corr_value = template_matching_SSD(resized_img_gray_tensor, self.recorded_img_tensor[i].unsqueeze(0).unsqueeze(0))
-                print(x, y, max_corr_value)
-                x_rect = int(x)
-                y_rect = int(y)
-
-                w_rect = int(self.recorded_img_tensor[i].shape[1])
-                h_rect = int(self.recorded_img_tensor[i].shape[0])
-                cv2.rectangle(resized_img_gray, (x_rect, y_rect), (x_rect + w_rect, y_rect + h_rect), (0, 255, 0), 2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 0.8
-                font_color = (0, 0, 255)
-                thickness = 2            
-                cv2.putText(resized_img_gray, str(max_corr_value), (x_rect, int(y_rect+h_rect/2)), font, font_scale, font_color, thickness)
+            w_rect = int(self.recorded_img_tensor[idx].shape[1])
+            h_rect = int(self.recorded_img_tensor[idx].shape[0])
+            cv2.rectangle(resized_img_gray, (x_rect, y_rect), (x_rect + w_rect, y_rect + h_rect), (0, 255, 0), 2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.8
+            font_color = (0, 0, 255)
+            thickness = 2            
+            cv2.putText(resized_img_gray, str(min_val), (x_rect, int(y_rect+h_rect/2)), font, font_scale, font_color, thickness)
+            cv2.putText(resized_img_gray, str(max_val), (x_rect, int(y_rect+h_rect/2+20)), font, font_scale, font_color, thickness)             
             
-            elif method=='cv2':
-                res = cv2.matchTemplate(resized_img_gray, self.recorded_img[i], cv2.TM_SQDIFF_NORMED)
-                min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-                top_left_xy = min_loc
-                x_rect = int(top_left_xy[0])
-                y_rect = int(top_left_xy[1])
+            original_center_x = (top_left_xy[0] + self.recorded_img[idx].shape[1] / 2) - self.col_bias_pct * resized_img_gray.shape[1]
+            original_center_y = (top_left_xy[1] + self.recorded_img[idx].shape[0] / 2) - self.row_bias_pct * resized_img_gray.shape[0]
 
-                w_rect = int(self.recorded_img_tensor[i].shape[1])
-                h_rect = int(self.recorded_img_tensor[i].shape[0])
-                cv2.rectangle(resized_img_gray, (x_rect, y_rect), (x_rect + w_rect, y_rect + h_rect), (0, 255, 0), 2)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 0.8
-                font_color = (0, 0, 255)
-                thickness = 2            
-                cv2.putText(resized_img_gray, str(min_val), (x_rect, int(y_rect+h_rect/2)), font, font_scale, font_color, thickness)
-                cv2.putText(resized_img_gray, str(max_val), (x_rect, int(y_rect+h_rect/2+20)), font, font_scale, font_color, thickness)             
-             
-                original_center_x = (top_left_xy[0] + self.recorded_img[i].shape[1] / 2) - self.col_bias_pct * resized_img_gray.shape[1]
-                original_center_y = (top_left_xy[1] + self.recorded_img[i].shape[0] / 2) - self.row_bias_pct * resized_img_gray.shape[0]
-
-                x_distance = resized_img_gray.shape[1] / 2 - original_center_x
-                y_distance = resized_img_gray.shape[0] / 2 - original_center_y
-                print("x_distance:",x_distance)
-                print("y_distance:",y_distance)
-                # print(x_distance, y_distance)
+            x_distance = -resized_img_gray.shape[1] / 2 + original_center_x
+            y_distance = -resized_img_gray.shape[0] / 2 + original_center_y
 
             resized_img_gray_msg = self.bridge.cv2_to_imgmsg(resized_img_gray)
 
@@ -260,24 +232,28 @@ class LfD_image(LfD):
 
             transform_base_2_cam = self.get_transform('panda_link0', 'camera_color_optical_frame')
             transform_correction = np.identity(4)
-            correction_increment = 0.0001
-            if abs(x_distance) > 5:
-                print("x distance greater")
+            correction_increment = 0.0004
+            if abs(x_distance) > self.x_dist_threshold:
                 transform_correction[0, 3] = np.sign(x_distance) * correction_increment
-            if abs(y_distance) > 5:
-                print("y distance greater")
+            if abs(y_distance) > self.y_dist_threshold:
                 transform_correction[1, 3] = np.sign(y_distance) * correction_increment
             transform = transform_base_2_cam @ transform_correction @ np.linalg.inv(transform_base_2_cam)
+            transform[2,3] = 0   # ignore z translation (in final transform/pose in base frame)
 
             corrected_pos = position_2_array(goal.pose.position) + transform[:3,3]
-            corrected_goal = array_quat_2_pose(corrected_pos, orientation_2_quaternion(goal.pose.orientation))
-            # diff_pos = corrected_pos - self.recorded_traj[:, i]
+            corrected_quat = orientation_2_quaternion(goal.pose.orientation)
+            corrected_goal = array_quat_2_pose(corrected_pos, corrected_quat)
+            transform_rot = tuple([corrected_quat.x, corrected_quat.y, corrected_quat.z, corrected_quat.w])
+
+            self._tf_broadcaster.sendTransform(tuple(corrected_pos), transform_rot, rospy.Time.now(), 'new_goal', 'panda_link0')
+
             corrected_goal.header.frame_id = 'panda_link0'
             self.corrected_goal_pub.publish(corrected_goal)
             self.goal_pub.publish(goal)
-            if min_val < 0.05:
-                if int(transform_correction[0,3]) or (transform_correction[1,3]):
-                    print("applying correction")
+            if min_val < 0.3:
+                if abs(x_distance) > self.x_dist_threshold or abs(y_distance) > self.y_dist_threshold:
+                    print("applying correction ", x_distance, " ", y_distance)
+                    z_sum += transform[2,3]
                 self.recorded_traj = self.recorded_traj + transform[:3, 3].reshape((3,1))
 
             if self.force.z > 9 and bool(int(spiral_flag)):

manipulation_tasks_panda/trajectory_manager/scripts/transform.py
@@ -29,7 +29,9 @@ class Transform():
     def get_transform(self, source_frame, target_frame):
         while True:
             try:
-                rp_tr, rp_rt = self._tf_listener.lookupTransform(source_frame, target_frame, rospy.Time.now() - rospy.Duration(1))
+                now = rospy.Time.now()
+                self._tf_listener.waitForTransform(source_frame, target_frame, now, rospy.Duration(4.0))
+                rp_tr, rp_rt = self._tf_listener.lookupTransform(source_frame, target_frame, now)
                 break
             except Exception as e:
                 rospy.logwarn(e)
@@ -58,5 +60,14 @@ class Transform():
         pose_old=copy.deepcopy(pose)
         print("transforming", transform)
         pose = transform_pose(pose, transform)
-        pose.pose.position.z=pose_old.pose.position.z
+        pose_quat = orientation_2_quaternion(pose.pose.orientation)
+        pose.pose.orientation.z = 0
+        pose.pose.orientation.w = 0
+        new_magnitude = np.sqrt(pose_quat.x * pose_quat.x + pose_quat.y * pose_quat.y)
+        pose_quat.x = pose_quat.x / new_magnitude
+        pose_quat.y = pose_quat.y / new_magnitude
+        pose.pose.orientation.x = pose_quat.x
+        pose.pose.orientation.y = pose_quat.y
+
+        pose.pose.position.z=0.4841658147707774
         return pose