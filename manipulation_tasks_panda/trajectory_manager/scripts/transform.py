47c35e9b80ad14d77df70b3878e854b7162735e0
platonics <nano2598.r@gmail.com>
2023-04-25 13:47:58 +0200

    Added function to compute final transform to transform class

--- manipulation_tasks_panda/trajectory_manager/scripts/transform.py
+++ manipulation_tasks_panda/trajectory_manager/scripts/transform.py
@@ -1,7 +1,8 @@
-from manipulation_helpers.pose_transform_functions import orientation_2_quaternion, pose_st_2_transformation, position_2_array, array_quat_2_pose, transformation_2_pose, transform_pose, list_2_quaternion
+from manipulation_helpers.pose_transform_functions import orientation_2_quaternion, pose_st_2_transformation, position_2_array, array_quat_2_pose, transformation_2_pose, transform_pose, list_2_quaternion, transform_pos_ori
 import numpy as np
 import rospy
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Bool
 import tf
 import copy
 class Transform():
@@ -18,14 +19,16 @@ class Transform():
         self.pose_hand_2_cam = array_quat_2_pose(trans_hand_2_cam, rot_hand_2_cam)
         self.transform_hand_2_cam = pose_st_2_transformation(self.pose_hand_2_cam)
 
-        self.transform_box_sub = rospy.Subscriber("/box_transform", PoseStamped, self.transform_icp_callback)
+        trans_home_pose = np.array([0.30681743793194804, -0.0005964840257302781, 0.4838100689233732])
+        quat_home_pose = np.quaternion(0.0001316886225132278, 0.9999887362754397, -0.004173250786325315, -0.0005296947552688665)
+        self.home_pose = array_quat_2_pose(trans_home_pose, quat_home_pose)
+
+        trans_home_pose = np.array([0.307, 0.000, 0.586])
+        quat_home_pose = np.quaternion(-0.001, 1.000, -0.005, -0.002)
+        self.home_pose = array_quat_2_pose(trans_home_pose, quat_home_pose)
+
         self._tf_listener = tf.TransformListener()
 
-   
-    def transform_icp_callback(self, pose_icp):
-        self.pose_icp = pose_icp
-        # self.pose_icp.pose.position.z = 0
-     
     def get_transform(self, source_frame, target_frame):
         while True:
             try:
@@ -37,6 +40,27 @@ class Transform():
                 rospy.logwarn(e)
         transform = np.dot(tf.transformations.translation_matrix(rp_tr), tf.transformations.quaternion_matrix(rp_rt))
         return transform
+    
+    def transform_traj_ori(self, traj, ori, transform):
+        transformed_traj = np.empty_like(traj)
+        transformed_ori = np.empty_like(ori)
+        for i in range(traj.shape[1]):
+            transformed_traj[:,i], transformed_ori[:,i] = transform_pos_ori(traj[:, i], ori[:, i], transform)
+        return transformed_traj, transformed_ori
+
+    def compute_final_transform(self):
+        transform_new = self.get_transform('panda_link0', 'panda_hand')
+        transform_new[0, 3] = transform_new[0, 3] #- 0.005
+        transform_new[1, 3] = transform_new[1, 3] #+ 0.005
+        home_pose_matrix = pose_st_2_transformation(self.home_pose)
+        self.final_transform =  transform_new @ np.linalg.inv(home_pose_matrix)
+        print(self.final_transform)
+        return self.final_transform
+    
+    def compute_final_transform_marker(self):
+        self.aruco_old_transform = np.load(self._package_path + '/trajectories/aruco_old.npy')
+        self.aruco_new_transform = self.get_transform('panda_link0', 'marker')
+        self.final_transform = self.aruco_new_transform @ np.linalg.inv(self.aruco_old_transform)
     
     def transform(self, transformation_pose, pose):
         # transform_base_2_cam = self.transform_base_2_hand @ self.transform_hand_2_cam
