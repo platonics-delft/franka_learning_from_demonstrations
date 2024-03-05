e76b480e65ca3f6682cdb82c7b02ec36f987aa85
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-03-05 18:40:26 +0100

    updated the spiral serach function to have variable name that are more infomative of what the function is doing

--- manipulation_tasks_panda/trajectory_manager/scripts/insertion.py
+++ manipulation_tasks_panda/trajectory_manager/scripts/insertion.py
@@ -1,31 +1,34 @@
 from panda_ros.pose_transform_functions import orientation_2_quaternion, position_2_array, array_quat_2_pose
 import numpy as np
-
+import rospy 
 class Insertion():
     def __init__(self):
         super(Insertion, self).__init__()
-    def spiral_search(self, goal):
+    def spiral_search(self, goal, control_rate=30, force_min_exit=1): 
+        # force_min_exit in Newton. If the verical force is lower than this value, the spiral search is considered successful
+        r = rospy.Rate(control_rate)
+        max_spiral_time = 30 # seconds
+        increase_radius_per_second = 0.0005 # meters, distance from center of the spiral after 1 second
+        rounds_per_second = 1 # how many rounds does the spiral every second
+        dt = 1. / control_rate
         goal_init = position_2_array(goal.pose.position)
         pos_init = self.curr_pos
         ori_quat = orientation_2_quaternion(goal.pose.orientation)
         goal_pose = array_quat_2_pose(goal_init, ori_quat)
-        time_spiral = 0
+        time= 0
         spiral_success = False
-        spiral_width = 2 * np.pi
-        spiral_speed = 1. / 20.
-        self.set_stiffness(4000, 4000, 1000, 30, 30, 30, 0)
-        for i in range(3000):
-            spiral_width = 2 * np.pi   ######### Should we make this a class variable?
+        self.set_stiffness(4000, 4000, 1000, 30, 30, 30, 0) # get more compliant in z direction
+        for _ in range(max_spiral_time * control_rate):   
             goal_pose.pose.position.x = pos_init[0] + np.cos(
-                spiral_width * time_spiral) * 0.0005 * time_spiral  # What is the 0.02?
+                2 * np.pi *rounds_per_second*time) * increase_radius_per_second * time
             goal_pose.pose.position.y = pos_init[1] + np.sin(
-                spiral_width * time_spiral) * 0.0005 * time_spiral  # What is the 0.02?
+                2 * np.pi *rounds_per_second* time) * increase_radius_per_second * time
             self.goal_pub.publish(goal_pose)
-            if self.force.z <= 1: #np.abs(goal_init[2] - self.curr_pos[2]) < 0.001:
+            if self.force.z <= force_min_exit: 
                 spiral_success = True
                 break
-            time_spiral += spiral_speed
-            self.r.sleep()
+            time += dt
+            r.sleep()
         self.set_stiffness(4000, 4000, 4000, 30, 30, 30, 0)    
         offset_correction = self.curr_pos - goal_init
 
