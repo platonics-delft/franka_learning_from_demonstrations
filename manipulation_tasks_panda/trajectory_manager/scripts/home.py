a72de5d2752a78ae19d072ec48209c64c822395a
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-04-19 16:22:35 +0200

    added the possibility of sending the robot home ad diferent x, y, z position

--- manipulation_tasks_panda/trajectory_manager/scripts/home.py
+++ manipulation_tasks_panda/trajectory_manager/scripts/home.py
@@ -6,8 +6,10 @@ import sys
 if __name__ == "__main__":
 
     height = rospy.get_param('/homing_node/height')
+    front_offset = rospy.get_param('/homing_node/front_offset')
+    side_offset = rospy.get_param('/homing_node/side_offset')
     print(f"Desired height is: {height}")
     rospy.init_node("homing_node")
     panda=Panda()
-    panda.home(height=height)
+    panda.home(height=height , front_offset=front_offset, side_offset=side_offset)
     panda.offset_compensator(10)
